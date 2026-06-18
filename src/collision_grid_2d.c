efac42be96d25ade2a16a7b7ad5b830dd1299e93
Dan Caldwell <dan.caldwell.3d@outlook.com>
2026-06-19 09:02:41 +1000

    minor cleanup

--- src/collision_grid_2d.c
+++ src/collision_grid_2d.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdio.h>
 
 #define GRID_CELL_SIZE 16
 
@@ -19,18 +20,19 @@ typedef struct {
 // declarations
 
 int get_2d_collision_grid_size(const float size_x, const float size_y, const float cell_size);
-int get_2d_collision_grid_stride_x(const float size_x, const float cell_size);
+int get_2d_collision_grid_row_length(const float size_x, const float cell_size);
 Grid2D* new_2d_collision_grid(const float size_x, const float size_y, const float cell_size);
-void clear_2d_grid_cell(const Grid2D* grid, const int cell_index);
-void populate_2d_cell_neighbours(const Grid2D* grid, const int cell_index);
-void init_2d_collision_grid(const Grid2D* grid);
-void reset_2d_collision_grid(const Grid2D* grid);
-void add_item_index_to_grid(const Grid2D* grid, const int item_index, const int cell_index);
-bool is_cell_occupied(const Grid2D* grid, const int cell_index);
-int xy_location_to_grid_cell(const Grid2D* grid, const float x, const float y);
+void clear_2d_grid_cell(Grid2D* grid, const int cell_index);
+void populate_2d_cell_neighbours(Grid2D* grid, const int cell_index);
+void init_2d_collision_grid(Grid2D* grid);
+void reset_2d_collision_grid(Grid2D* grid);
+int add_item_index_to_grid(Grid2D* grid, const int item_index, const int cell_index);
+bool is_cell_occupied(Grid2D* grid, const int cell_index);
+int xy_location_to_grid_cell(Grid2D* grid, const float x, const float y);
+void free_2d_collision_grid(Grid2D* grid);
 
 
-int xy_location_to_grid_cell(const Grid2D* grid, const float x, const float y) {
+int xy_location_to_grid_cell(Grid2D* grid, const float x, const float y) {
     // returns the grid cell that corresponds to the given x and y locations
 
     int grid_x = (int)x / grid->cell_size;
@@ -60,7 +62,7 @@ int get_2d_collision_grid_size(const float size_x, const float size_y, const flo
     return num_cells;
 }
 
-int get_2d_collision_grid_stride_x(const float size_x, const float cell_size) {
+int get_2d_collision_grid_row_length(const float size_x, const float cell_size) {
     // returns the number of cells across the x axis
 
     const int x_cells = (int)ceilf(size_x / cell_size);
@@ -77,21 +79,34 @@ Grid2D* new_2d_collision_grid(const float size_x, const float size_y, const floa
 
     grid->cell_size = cell_size;
     grid->num_cells = get_2d_collision_grid_size(size_x, size_y, cell_size);
-    grid->grid_row_length = get_2d_collision_grid_stride_x(size_x, cell_size);
+    grid->grid_row_length = get_2d_collision_grid_row_length(size_x, cell_size);
 
     grid->grid_cells = (GridCell2D*)calloc(grid->num_cells, sizeof(GridCell2D));
+
+    if (!grid->grid_cells) {
+        free_2d_collision_grid(grid);
+        printf("Grid allocation failed");
+        return NULL;
+    }
+
+    init_2d_collision_grid(grid);
 
     return grid;
 }
 
-void clear_2d_grid_cell(const Grid2D* grid, const int cell_index) {
+void free_2d_collision_grid(Grid2D* grid) {
+    free(grid->grid_cells);
+    free(grid);
+}
+
+void clear_2d_grid_cell(Grid2D* grid, const int cell_index) {
     // sets all sub-cells to -1 in the given grid cell
     for (int i = 0; i < GRID_CELL_SIZE; i++) {
         grid->grid_cells[cell_index].cell[i] = -1;
     }
 }
 
-void populate_2d_cell_neighbours(const Grid2D* grid, const int cell_index) {
+void populate_2d_cell_neighbours(Grid2D* grid, const int cell_index) {
     /* cell neighbour order
      * 0, 1, 2
      * 3, *, 4
@@ -139,34 +154,40 @@ void populate_2d_cell_neighbours(const Grid2D* grid, const int cell_index) {
     }
 }
 
-void init_2d_collision_grid(const Grid2D* grid) {
+void init_2d_collision_grid(Grid2D* grid) {
     // set all grid cells to -1 (empty) and initialise neighbour grid lookups
     for (int i = 0; i < grid->num_cells; i++) {
         clear_2d_grid_cell(grid, i);
         populate_2d_cell_neighbours(grid, i);
     }
 }
-void reset_2d_collision_grid(const Grid2D* grid) {
+void reset_2d_collision_grid(Grid2D* grid) {
     // set all grid cells to -1 (empty)
     for (int i = 0; i < grid->num_cells; i++) {
         clear_2d_grid_cell(grid, i);
     }
 }
 
-void add_item_index_to_grid(const Grid2D* grid, const int item_index, const int cell_index) {
-    // adds an item index to the given grid cell
+int add_item_index_to_grid(Grid2D* grid, const int item_index, const int cell_index) {
+    // adds an item index to the given grid cell. returns 0 if successful,
+    // 1 if item or cell index are out of range, or 2 if grid cell is full
+    if (item_index < 0 | cell_index < 0 | cell_index > grid->num_cells)
+    {
+        return 1;
+    }
 
     for (int i = 0; i < GRID_CELL_SIZE; i++) {
 
         if (grid->grid_cells[cell_index].cell[i] == -1) {
             grid->grid_cells[cell_index].cell[i] = item_index;
-            return;
+            return 0;
         }
     }
+    return 2;
 }
 
-bool is_cell_occupied(const Grid2D* grid, const int cell_index) {
-    // checks if the first sub-cell of a grid cell is empty (-1)
+bool is_cell_occupied(Grid2D* grid, const int cell_index) {
+    // returns true if the first sub-cell is occupied
 
     if (grid->grid_cells[cell_index].cell[0] != -1) {
         return true;
